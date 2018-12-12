1280a5c89cd6c0b10af5c3183db44167c1cb7488
hbtb <bishehsary@gmail.com>
2018-12-12 00:55:04 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { Indexer, Packager } = require("@vesta/devmaid");
+const gulp = require("gulp")
+
+let pkgr = new Packager({
+    root: __dirname,
+    src: "src",
+    targets: ["es6"],
+    files: [".npmignore", "LICENSE", "README.md"],
+    publish: "--access=public",
+});
+
+function indexer() {
+    const indexer = new Indexer("src/components");
+    indexer.generate();
+    return Promise.resolve();
+}
+
+function watch() {
+    gulp.watch(`src/scss/**/*`, indexer);
+    return Promise.resolve();
+}
+
+const tasks = pkgr.createTasks();
+module.exports = {
+    default: gulp.series(indexer, tasks.default, watch),
+    publish: gulp.series(indexer, tasks.deploy, tasks.publish)
+}
